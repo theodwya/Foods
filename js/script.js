d0b38b8bb7fdbcca1d105cfb4ee54273a2fe9452
theodwya <navyteddy5286@gmail.com>
2025-03-03 18:20:29 -0500

    Added Code for Navigation Bar in HTML, CSS, and new JS for getting Navigation Bar to be responsive to clicks

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+document.querySelector('.menu').addEventListener('click', ()=>{
+    document.querySelectorAll('.target').forEach((item)=>{
+        item.classList.toggle('change');
+    });
+});
+
 const icons = document.querySelectorAll('.main-section-icons i');
 let currentIndex = 0;
 
